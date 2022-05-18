d835dc3ca48d53749a556b6b6dab228ff3b1115d
Mayur <ultramayur123@gmail.com>
2022-05-18 12:01:06 +0530

    RMX2170: Blaze initial commit
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- blaze_RMX2170.mk
+++ blaze_RMX2170.mk
@@ -7,14 +7,14 @@
 # Inherit from RMX2170 device
 $(call inherit-product, device/realme/RMX2170/device.mk)
 
-# Inherit some common EvolutionX stuff
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common ProjectBlaze stuff
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := evolution_RMX2170
+PRODUCT_NAME := blaze_RMX2170
 PRODUCT_DEVICE := RMX2170
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 7 Pro
@@ -31,16 +31,11 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX2170" \
     TARGET_DEVICE="RMX2170"
 
-# Inherit EvolutionX build stuff.
+# Inherit ProjectBlaze build stuff.
 WITH_GAPPS := true
 TARGET_GAPPS_ARCH := arm64
-TARGET_BOOT_ANIMATION_RES_EVO := true
 TARGET_PIXEL_CHARGE_ANIM := true
 TARGET_SUPPORTS_QUICK_TAP := true
 EXTRA_UDFPS_ANIMATIONS := true
-EVO_BUILD_TYPE := OFFICIAL
-
-# Memetainer stuff
-EVO_DONATE_URL := https://paypal.me/marshmello61
-EVO_MAINTAINER := Mayur-Varde
-EVO_SUPPORT_URL := https://t.me/mellolab
+BLAZE_MAINTAINER := Mayur Varde
+TARGET_FACE_UNLOCK_SUPPORTED := true
