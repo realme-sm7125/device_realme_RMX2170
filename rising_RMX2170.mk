9fe824cefc7f071316385945a4cfa054ee2d9e1e
Mayur <ultramayur123@gmail.com>
2023-11-03 06:46:35 +0000

    RMX2170: initial rising commit
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- rising_RMX2170.mk
+++ rising_RMX2170.mk
@@ -7,14 +7,14 @@
 # Inherit from RMX2170 device
 $(call inherit-product, device/realme/RMX2170/device.mk)
 
-# Inherit some common DerpFest stuff
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common risingOS stuff
+$(call inherit-product, vendor/rising/config/rising.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := derp_RMX2170
+PRODUCT_NAME := rising_RMX2170
 PRODUCT_DEVICE := RMX2170
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 7 Pro
@@ -37,15 +37,23 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT="RMX2170" \
     TARGET_DEVICE="RMX2170"
 
-# Inherit DerpFest build stuff.
-WITH_GAPPS := true
+# Inherit risingOS build stuff.
+WITH_GMS := true
 TARGET_GAPPS_ARCH := arm64
 IS_PHONE := true
-DERP_VERSION_APPEND_TIME_OF_DAY := true
 TARGET_PIXEL_CHARGE_ANIM := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_INCLUDE_STOCK_ARCORE := true
-DERP_BUILDTYPE := Official
+TARGET_HAS_UDFPS := true
 EXTRA_UDFPS_ANIMATIONS := true
-USE_LEGACY_BOOTANIMATION := true
+TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_USE_GOOGLE_TELEPHONY := true
+
+# Inherit risingOS prop stuff
+RISING_MAINTAINER := marshmello61
+RISING_CHIPSET := Snapdragon
+RISING_STORAGE := 128gb
+RISING_RAM := 8gb
+RISING_BATTERY := 4500mah
+RISING_DISPLAY := 2400×1080
